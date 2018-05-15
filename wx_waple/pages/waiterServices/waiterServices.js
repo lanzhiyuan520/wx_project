534a0b6d52e5018cc61ffb3ffe71856a601850a7
lanzhiyuan <lanzhiyuan99@126.com>
2018-05-15 22:26:05 +0800

    轮播实现

--- wx_waple/pages/waiterServices/waiterServices.js
+++ wx_waple/pages/waiterServices/waiterServices.js
@@ -2,15 +2,17 @@ const app = getApp()
 var sideBarstart
 Page({
     data: {
-        left:-520,
+        left:0,
         work_list:[
             {},
             {},
             {},
             {},
             {},
+            {}
         ],
-        active:2
+        active:0,
+        current:0
     },
     pageTouchStartHandler:function(e){
         sideBarstart = e.changedTouches[0].pageX
@@ -20,7 +22,9 @@ Page({
         var touchend = e.changedTouches[0].pageX
         // slip left
         if (touchend - sideBarstart < -50) {
-
+            if (this.data.active > this.data.work_list.length-1){
+                return false
+            }
            var offset_left = this.data.active * 520
             this.setData({
                 active : this.data.active+1,
@@ -29,12 +33,8 @@ Page({
         }
         // slip right
         if (touchend - sideBarstart > 50) {
-            if(this.data.active <= 1){
+            if(this.data.active < 2){
                 console.log(this.data.active)
-                this.setData({
-                    active : 5,
-                    left : -2080
-                })
                 return false
             }
             var offset_right = this.data.left + 520
@@ -44,6 +44,11 @@ Page({
                 left : offset_right
             })
         }
+    },
+    change_c:function(e){
+        this.setData({
+            current : e.detail.current
+        })
     },
     onLoad: function () {
 
