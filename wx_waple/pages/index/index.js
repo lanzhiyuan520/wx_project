1de483ffbc780da154c7cb1c34408f0be06ebd8f
兰兄 <lanxiong@lanxiongdeiMac.lan>
2018-05-17 10:59:42 +0800

    样式修改

--- wx_waple/pages/index/index.js
+++ wx_waple/pages/index/index.js
@@ -16,9 +16,33 @@ Page({
           {img:'http://cdn.ayi800.com/image/1aedf6b47bda6cccda6602c4fd2de4b5.jpg',name:"马冬梅",price:'19200元/26天'},
           {img:'http://cdn.ayi800.com/image/1aedf6b47bda6cccda6602c4fd2de4b5.jpg',name:"马冬梅",price:'19200元/26天'}
       ],
+      comment_list:[
+          {
+              name:'杨珍珠',
+              time : '4月15日',
+              content:'马东什么阿姨很不错，照顾新生儿很有经验照顾新生儿很有经验，烧的催乳汤巨好喝，符合口味很耐思，月子餐什么啊的',
+              waiter_name:'什么梅',
+              price:'9800/26天'
+          },
+          {
+              name:'杨珍珠',
+              time : '4月15日',
+              content:'马东什么阿姨很不错，照顾新生儿很有经验照顾新生儿很有经验，烧的催乳汤巨好喝，符合口味很耐思，月子餐什么啊的',
+              waiter_name:'什么梅',
+              price:'9800/26天'
+          },
+          {
+              name:'杨珍珠',
+              time : '4月15日',
+              content:'马东什么阿姨很不错，照顾新生儿很有经验照顾新生儿很有经验，烧的催乳汤巨好喝，符合口味很耐思，月子餐什么啊的',
+              waiter_name:'什么梅',
+              price:'9800/26天'
+          }
+      ],
       city_list:[{name:'北京'},{name:'广州'},{name:'深圳'}],
       city_list_height:true,
-      city_name:'北京'
+      city_name:'北京',
+      pull_text:'上拉加载更多'
   },
     service:function(e){
       if (e.currentTarget.dataset.idx == 0){
@@ -49,6 +73,32 @@ Page({
         wx.switchTab({
             url: '../waiter/waiter'
         })
+    },
+    onReachBottom:function () {
+        var that = this
+        this.setData({
+            pull_text : '加载中...'
+        })
+        wx.showLoading({
+            title : '加载中',
+            mask : true
+        })
+        var arr = this.data.comment_list
+
+        setTimeout(()=>{
+            arr.push({
+                name:'杨珍珠',
+                time : '4月15日',
+                content:'马东什么阿姨很不错，照顾新生儿很有经验照顾新生儿很有经验，烧的催乳汤巨好喝，符合口味很耐思，月子餐什么啊的',
+                waiter_name:'什么梅',
+                price:'9800/26天'
+            })
+            that.setData({
+                comment_list : arr,
+                pull_text : '上拉加载更多'
+            })
+            wx.hideLoading()
+        },2000)
     },
   onLoad: function () {
 
